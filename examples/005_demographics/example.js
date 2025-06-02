c1a32f68c4eb7658e1d60701ed3438d3a0541ba7
Daniel Barker <phuebu@googlemail.com>
2025-06-02 10:24:35 +0100

    use new animation api

--- examples/005_demographics/example.js
+++ examples/005_demographics/example.js
@@ -1,27 +1,25 @@
 // @ts-check
 
 import { rect } from "../../dist/2d/shapes.js";
-import { Interactive, ViewPort2D } from "../../dist/2d/view.js";
+import { Interactive, setup_canvas, ViewPort2D } from "../../dist/2d/view.js";
 import { AnimationController } from "../../dist/anim.js";
 import { annotate_labeled_ticks, annotate_text, arange, Clip, Composite, Constant, cumprod, cumsum, draw_axes, draw_axis_grid, draw_func, draw_h_line, draw_plot, draw_poly, draw_rectangle, EaseInOut, indices, LoopN, Rescale, stroke_default, sum, Tween, vec2 } from "../../dist/mint.js";
 
 /** @type {HTMLCanvasElement} */
 let canvas = document.getElementById("theCanvas");
 
-// Ensure buffer matches the layed-out size.
-// canvas.style.width = canvas.clientWidth + "px";
-// canvas.style.height = canvas.clientHeight + "px";
-// canvas.width = canvas.clientWidth * 1.5;
-// canvas.height = canvas.clientHeight * 1.5;
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
-
 /** @type {CanvasRenderingContext2D} */
 let ctx = canvas.getContext("2d");
 
+setup_canvas(ctx);
+
 /** @type {HTMLInputElement} */
 let exogSlider = document.getElementById("exogBirthRateSlider");
+
+/** @type {HTMLInputElement} */
 let endogSlider = document.getElementById("endogBirthRateSlider");
+
+/** @type {HTMLInputElement} */
 let hazardSlider = document.getElementById("hazardRateSlider");
 
 // The viewport to the canvas.
@@ -97,14 +95,11 @@ function draw(anim) {
         }
         tot[tot.length - 1] = sum(pop);
         years += (amax / xmax);
-        // years = years % (exog_anim.duration);
     }
 
 
     const ytop = canvas.height - 1.2 * cpad;
     let view_tot = new ViewPort2D(ctx, rect([years - tot.length, years - 1], [0, 300]), rect([1.2 * cpad, 1.2 * cpad + 200], [ytop - 100, ytop]))
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     draw_axis_grid(view_pop, 2, 0.2, { linecolor: "#DDDDDD" })
     const xticks = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
@@ -130,7 +125,6 @@ function draw(anim) {
     });
 
     draw_rectangle(view_pop, view_pop.data);
-    // vec2(-2, view_pop.data.width / 2.0)
     annotate_text(view_pop, "Age", vec2(view_pop.data.width / 2.0, -0.22), ".-");
     annotate_text(view_pop, "Population", vec2(-0.5, view_pop.data.height / 2.0), ".+", 90, { fillcolor: "steelblue" });
     annotate_text(view_pop, "Hazard Rate", vec2(view_pop.data.width + 2.5, view_pop.data.height * 0.75), ".+", -90, { fillcolor: "coral" });
@@ -153,8 +147,8 @@ function draw(anim) {
     // annotate_text(view_tot, "Time", view_tot.data.center.map_y((y) => -y * 0.1), ".-", 0, { fillcolor: "black", linecolor: "white", linewidth: 3 });
 }
 
-const anim = new AnimationController()
-anim.start(draw);
+const anim = new AnimationController(ctx, draw)
+anim.start();
 
 exogSlider.addEventListener("mousedown", (e) => {
     exogAnim = null;
